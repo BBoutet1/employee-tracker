dc240b48aff5ce322391c662dfdaab38a5245a3d
Brice Boutet <boutetlb@gmail.com>
2020-06-11 22:56:43 -0400

    viewEmployees() function working

--- server.js
+++ server.js
@@ -17,13 +17,15 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
+    console.log("connected as id " + connection.threadId);
     manageEmployees();
 });
 // Answers to the first inquirer question
 whatToDo = ["View all employees", "View all employees by department",
     "View all employees by manager", "Add employee", "Remove employee",
-    "Update employee role", "Update employee Manager"
+    "Update employee role", "Update employee Manager", "Add role", "Add department",
 ];
+
 
 function manageEmployees() {
     inquirer
@@ -34,7 +36,35 @@ function manageEmployees() {
             choices: whatToDo,
         }])
         .then(function(answer) {
+            if (answer.doWhat === whatToDo[0]) {
+                viewEmployees();
+            } else if (answer.doWhat === whatToDo[1]) {
+                EmployeesbyDepatment();
+            } else if (answer.doWhat === whatToDo[2]) {
+                EmployeesbyManager();
+            } else if (answer.doWhat === whatToDo[3]) {
+                addEmployee();
+            } else if (answer.doWhat === whatToDo[4]) {
+                removeEmployee();
+            } else if (answer.doWhat === whatToDo[5]) {
+                updateRole();
+            } else if (answer.doWhat === whatToDo[6]) {
+                updateManager();
+            } else if (answer.doWhat === whatToDo[7]) {
+                addRole();
+            } else if (answer.doWhat === whatToDo[6]) {
+                addDepartment();
+            }
 
         });
-    console.log("App listening on PORT");
+
+    function viewEmployees() {
+        connection.query("SELECT * FROM employee", function(err, res) {
+            if (err) throw err;
+            console.log(res);
+            connection.end();
+        });
+    }
+
+
 }
