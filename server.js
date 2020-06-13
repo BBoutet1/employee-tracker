dccae0bff86384bf2e2cc1c166a90e6289ae033d
Brice Boutet <boutetlb@gmail.com>
2020-06-13 18:40:38 -0400

    View employees by department: Answer from list instead of input

--- server.js
+++ server.js
@@ -20,6 +20,12 @@ connection.connect(function(err) {
     console.log("connected as id " + connection.threadId);
     manageEmployees();
 });
+
+// Global variable;
+let departmentsList = [];
+let managersList = [];
+let rolesList = [];
+
 // Answers to the first inquirer question
 actionToDo = ["View all employees", "View all employees by department",
     "View all employees by manager", "Add employee", "Remove employee",
@@ -42,7 +48,10 @@ function manageEmployees() {
                     break;
 
                 case actionToDo[1]:
-                    employeesbyDepatment();
+                    viewEmployeesBy("department", function(res) {
+                        res.forEach(department => departmentsList.push(department.name));
+                        employeesByDepatment(departmentsList)
+                    });
                     break;
 
                 case actionToDo[2]:
@@ -71,9 +80,15 @@ function manageEmployees() {
             }
 
         });
-
-
-
+    //A function to return an array listing departments, managers or roles
+    function viewEmployeesBy(param, getTable) {
+        //Database query to get all departments names
+        connection.query("SELECT * FROM " + param, function(err, res) {
+            if (err) throw err;
+            // deparmentsList = res;
+            return getTable(res)
+        });
+    }
 
     function viewEmployees() {
         connection.query("SELECT * FROM employee", function(err, res) {
@@ -83,19 +98,18 @@ function manageEmployees() {
         });
     }
 
-    function employeesbyDepatment() {
-
+    function employeesByDepatment(departmentsList) {;
         inquirer
             .prompt({
+                message: "Which department employee would do like to see?",
                 name: "department",
-                type: "input",
-                message: "Which department employee would do like to see?"
+                type: "list",
+                choices: departmentsList
             })
             .then(function(answer) {
-                var query = "SELECT employee.first_name, employee.last_name ";
+                let query = "SELECT employee.first_name, employee.last_name ";
                 query += "FROM employee INNER JOIN role ON (employee.role_id = role.id) INNER JOIN department ON (role.department_id = department.id) ";
                 query += "WHERE department.name = ?";
-
                 connection.query(query, answer.department, function(err, res) {
                     if (err) throw err;
                     console.log(res.length + " matches found!");
@@ -104,6 +118,5 @@ function manageEmployees() {
                 });
             });
     }
-
 
 }
